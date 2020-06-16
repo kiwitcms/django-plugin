932f87ac167b7ac1c54657631b3a163a674e8592
Bryan Mutai <mutaiwork@gmail.com>
2020-06-16 15:39:02 +0300

    Resolves #20. Adds comments to Kiwi from Test run with --debug-sql

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,26 +1,30 @@
 from unittest import skip, expectedFailure
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
+from .models import PageInfo
 
 
-class TestAppContactPageTest(SimpleTestCase):
+class TestAppContactPageTest(TestCase):
     def test_contact_page(self):
+        PageInfo.objects.filter(page_title="pass").count()
         url = reverse('contact')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
 
-class TestAppAboutPageTest(SimpleTestCase):
+class TestAppAboutPageTest(TestCase):
     # pylint: disable=unreachable
     def test_about_page(self):
+        PageInfo.objects.filter(page_title="error").count()
         url = reverse('about')
         response = self.client.get(url)
         raise Exception
         self.assertEqual(response.status_code, 200)
 
 
-class TestAppIndexPageTest(SimpleTestCase):
+class TestAppIndexPageTest(TestCase):
     def test_index_page(self):
+        PageInfo.objects.filter(page_title="pass").count()
         url = reverse('index')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -30,8 +34,9 @@ class TestAppIndexPageTest(SimpleTestCase):
         self.fail("shouldn't have run")
 
 
-class TestAppMissingPageTest(SimpleTestCase):
+class TestAppMissingPageTest(TestCase):
     def test_missing_page(self):
+        PageInfo.objects.filter(page_title="fail").count()
         response = self.client.get('/testapp/missing')
         self.assertEqual(response.status_code, 200)
 
@@ -44,7 +49,7 @@ class TestAppMissingPageTest(SimpleTestCase):
         self.assertEqual(1, 1, "broken (not actually)")
 
 
-class TestAppBooleanPageTest(SimpleTestCase):
+class TestAppBooleanPageTest(TestCase):
     def test_get_correct_boolean(self):
         with self.subTest("Should return False"):
             false_response = self.client.get('/testapp/boolean/0/')
@@ -53,5 +58,6 @@ class TestAppBooleanPageTest(SimpleTestCase):
             true_response = self.client.get('/testapp/boolean/1/')
             self.assertEqual(true_response.content, b'True')
         with self.subTest("Should return invalid"):
+            PageInfo.objects.filter(page_title="fail").count()
             invalid_response = self.client.get('/testapp/boolean/99/')
             self.assertEqual(invalid_response.content, b'invalid')
