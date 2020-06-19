871026735fb232c1e044a5a1d97b94d890f4b483
Mr. Senko <atodorov@mrsenko.com>
2020-06-19 21:30:43 +0300

    tests: Rename test methods based on their expected result

--- testapp/tests.py
+++ testapp/tests.py
@@ -5,7 +5,7 @@ from .models import PageInfo
 
 
 class TestAppContactPageTest(TestCase):
-    def test_contact_page(self):
+    def test_that_will_report_passed(self):
         PageInfo.objects.filter(page_title="pass").count()
         url = reverse('contact')
         response = self.client.get(url)
@@ -14,7 +14,7 @@ class TestAppContactPageTest(TestCase):
 
 class TestAppAboutPageTest(TestCase):
     # pylint: disable=unreachable
-    def test_about_page(self):
+    def test_that_will_report_error(self):
         PageInfo.objects.filter(page_title="error").count()
         url = reverse('about')
         response = self.client.get(url)
@@ -30,27 +30,27 @@ class TestAppIndexPageTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     @skip("demonstrating skipping")
-    def test_nothing(self):
+    def test_that_will_report_waived(self):
         self.fail("shouldn't have run")
 
 
 class TestAppMissingPageTest(TestCase):
-    def test_missing_page(self):
+    def test_that_will_report_failed(self):
         PageInfo.objects.filter(page_title="fail").count()
         response = self.client.get('/testapp/missing')
         self.assertEqual(response.status_code, 200)
 
     @expectedFailure
-    def test_fail(self):
+    def test_expected_failure_reported_as_passed(self):
         self.assertEqual(1, 0, "broken")
 
     @expectedFailure
-    def test_unexpected_success(self):
+    def test_unexpected_success_reported_as_failed(self):
         self.assertEqual(1, 1, "broken (not actually)")
 
 
 class TestAppBooleanPageTest(TestCase):
-    def test_get_correct_boolean(self):
+    def test_which_demonstrates_subtests(self):
         with self.subTest("Should return False"):
             false_response = self.client.get('/testapp/boolean/0/')
             self.assertEqual(false_response.content, b'False')
