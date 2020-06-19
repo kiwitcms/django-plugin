38455653ca807dadfc215a57b64d35e87c771aba
Mr. Senko <atodorov@mrsenko.com>
2020-06-19 21:37:53 +0300

    tests: Rearrange the methods
    just becuse I find this arrangement easier to follow

--- testapp/tests.py
+++ testapp/tests.py
@@ -4,53 +4,39 @@ from django.urls import reverse
 from .models import PageInfo
 
 
-class TestAppContactPageTest(TestCase):
-    def test_that_will_report_passed(self):
-        PageInfo.objects.filter(page_title="pass").count()
-        url = reverse('contact')
-        response = self.client.get(url)
+class TestWhichReportsVariousStatuses(TestCase):
+    def test_will_report_failed(self):
+        PageInfo.objects.filter(page_title="fail").count()
+        response = self.client.get('/testapp/missing')
         self.assertEqual(response.status_code, 200)
 
+    @expectedFailure
+    def test_unexpected_success_will_report_failed(self):
+        self.assertEqual(1, 1, "broken (not actually)")
 
-class TestAppAboutPageTest(TestCase):
     # pylint: disable=unreachable
-    def test_that_will_report_error(self):
+    def test_will_report_error(self):
         PageInfo.objects.filter(page_title="error").count()
         url = reverse('about')
         response = self.client.get(url)
         raise Exception
         self.assertEqual(response.status_code, 200)
 
+    @skip("demonstrating skipping")
+    def test_will_report_waived(self):
+        self.fail("shouldn't have run")
 
-class TestAppIndexPageTest(TestCase):
-    def test_index_page(self):
+    def test_will_report_passed(self):
         PageInfo.objects.filter(page_title="pass").count()
-        url = reverse('index')
+        url = reverse('contact')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    @skip("demonstrating skipping")
-    def test_that_will_report_waived(self):
-        self.fail("shouldn't have run")
-
-
-class TestAppMissingPageTest(TestCase):
-    def test_that_will_report_failed(self):
-        PageInfo.objects.filter(page_title="fail").count()
-        response = self.client.get('/testapp/missing')
-        self.assertEqual(response.status_code, 200)
-
     @expectedFailure
-    def test_expected_failure_reported_as_passed(self):
+    def test_expected_failure_will_report_passed(self):
         self.assertEqual(1, 0, "broken")
 
-    @expectedFailure
-    def test_unexpected_success_reported_as_failed(self):
-        self.assertEqual(1, 1, "broken (not actually)")
-
-
-class TestAppBooleanPageTest(TestCase):
-    def test_which_demonstrates_subtests(self):
+    def test_with_subtests_will_report_failed(self):
         with self.subTest("Should return False"):
             false_response = self.client.get('/testapp/boolean/0/')
             self.assertEqual(false_response.content, b'False')
@@ -61,3 +47,8 @@ class TestAppBooleanPageTest(TestCase):
             PageInfo.objects.filter(page_title="fail").count()
             invalid_response = self.client.get('/testapp/boolean/99/')
             self.assertEqual(invalid_response.content, b'invalid')
+
+    def test_with_subtests_will_report_passed(self):
+        with self.subTest("Should pass"):
+            PageInfo.objects.filter(page_title="pass").count()
+            self.assertEqual(1, 1)
